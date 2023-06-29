cef0a076578e74218d06b484b8806d66127253c4
Jan Kožusznik <jan@kozusznik.cz>
2023-06-29 20:07:06 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,3 @@ git clone https://github.com/kozusznik/dotfiles $HOME/.dotfiles
 git -C $HOME/.dotfiles/ submodule init
 git -C $HOME/.dotfiles/ submodule update
 $HOME/.dotfiles/fresh.sh
-. $HOME/.zshrc
